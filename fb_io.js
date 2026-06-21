9c7f8b54d0658c58b4d0775ff43b1edbbddb0030
tom barker <23197tb@hvhs.school.nz>
2026-06-22 11:04:10 +1200

    22/06/26 button buttoning my buttons like buttoning my buttons

--- fb_io.js
+++ fb_io.js
@@ -7,8 +7,8 @@
  **************************************************************
  **************************************************************/
 
- var GLOBAL_user;  // Google's user object
-
+var GLOBAL_user;  // Google's user object
+let UID;
 // Set up a listener for the login state of the user.
 function fb_authenticate() {
   authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
@@ -19,8 +19,8 @@ function fb_handleLogin(_user) {
   if (_user) {
     console.log("User is logged in")
     GLOBAL_user = _user; // Save the user object to a global variable
-     document.getElementById("alexispissesmeoff").hidden = true
-    
+    document.getElementById("alexispissesmeoff").hidden = true
+    console.log(_user)
   } else {
     console.log("User is NOT logged in - Starting the popup process")
     fb_popupLogin();
@@ -34,10 +34,57 @@ function fb_popupLogin() {
   firebase.auth().signInWithPopup(provider).then((result) => {
     GLOBAL_user = result.user;  // Save the user object to a global variable
     console.log("User has logged in")
+    UID = GLOBAL_user.uid
   });
 }
 
 function fb_error(error) {
   console.log("There was an error reading the message");
   console.error(error);
+}
+console.log("Running too cool for skl")
+const HTML_OUTPUT = document.getElementById("statusMessage");
+let userAge
+let userGameName
+async function writeForm() {
+  let UID = GLOBAL_user.uid
+  let email = GLOBAL_user.email
+  let userPFP = GLOBAL_user.photoURL
+  const userAge = document.getElementById("userAge").value;
+  const userGameName = document.getElementById("userGameName").value;
+
+  firebase.database().ref('/' + UID).set(
+    {
+      userAge,
+      userGameName,
+      email
+
+    }
+  )
+  var snapshot = await firebase.database().ref('/' + UID).once('value');
+  data = snapshot.val()
+  console.log("Running displayRead(), the message is: " + snapshot.val())
+}
+
+
+function display(snapshot) {
+  var dbData = snapshot.val();
+  if (dbData === null) { // if there is no data, dbData will be null.
+    console.log('There was no record when trying to read the message');
+  }
+  else {
+    // console.log("The message is: " + dbData)
+  }
+}
+function homePage() {
+  if (UID === null) {
+    alert("Please log in.");
+  }
+  else {
+    window.location.href = "gameSelection.html";
+    console.log("help")
+  }
+}
+function goGeoDash() {
+  window.location.href = "GeoDash/GeoDash game/index.html";
 }
